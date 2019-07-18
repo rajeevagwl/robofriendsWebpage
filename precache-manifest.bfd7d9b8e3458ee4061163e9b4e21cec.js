77354d82e8b247abde84d617a4553fc1791c13b6
Rajeev Agrawal <rajeev.agwl@gmail.com>
2019-07-18 17:56:09 +0530

    robofriends build

---
+++ precache-manifest.bfd7d9b8e3458ee4061163e9b4e21cec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81043861cfb89e83d45e94f8ab7b0bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "40e457af5f309fc068a5",
+    "url": "/static/css/main.b649db81.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "40e457af5f309fc068a5",
+    "url": "/static/js/main.2620ae37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
